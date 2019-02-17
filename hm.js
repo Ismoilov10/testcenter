5f09092d44ded2f54ed719c980f4a33b5e1d2da8
Ismoilov10 <36671419+Ismoilov10@users.noreply.github.com>
2019-02-17 19:05:38 +0500

    Add files via upload

---
+++ hm.js
@@ -0,0 +1,13 @@
+$(function(){
+
+			$("body").on("click",".btn",function(){
+				$(".sar").animate({right:680,bottom:400},2000);
+				$(".gardan").animate({left:651,bottom:390},2000);
+				$(".tan").animate({left:628,bottom:264},2000);
+				$(".dastr").animate({right:638,bottom:320},2000);
+				$(".dastl").animate({right:748,bottom:320},2000);
+				$(".pochar").animate({left:638,bottom:200},2000);
+				$(".pochal").animate({left:668,bottom:200},2000);
+
+			});
+});
